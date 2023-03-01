97127fbb46b9ae836903807477f6c1716a9d5c0f
LIL-KOTYO <52920453+LILKOTYO@users.noreply.github.com>
2023-03-01 20:13:05 +0800

    blog update 20230301

---
+++ docs/pbd-series/pbd-xpbd-framework.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+---
+
+# PBD, XPBD仿真框架
+intro...
+
+## PBD
+$\dot{\omega}_i={\bf I}_i^{-1}(\tau_i-(\omega_i\times ({\bf I}_i\omega_i)))$
+
+## XPBD
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,6 +4,21 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
+    title: '你好!👋',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        我是Ryao，目前在上海交通大学控制科学与工程专业攻读硕士学位。 <br />
+
+        我目前主要研究方向是计算机图形学（模拟、渲染）和数字孪生建模。 <br />
+
+        我的邮箱：lilkotyo@gmail.com, lil-kotyo@sjtu.edu.cn. <br />
+
+        如果你对图形学感兴趣，欢迎联系我！ <br />
+      </>
+    ),
+  },
+  {
     title: 'Hi there 👋',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
@@ -22,7 +37,7 @@ const FeatureList = [
 
 function Feature({Svg, title, description}) {
   return (
-    <div className={clsx('col')}>
+    <div className={clsx('col col--6')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
